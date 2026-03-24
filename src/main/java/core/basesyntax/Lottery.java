16e76be1fa2f0f59b63a3e23ca2a86895ce580a0
Maksim Golyak <maksimgoliak@icloud.com>
2026-03-24 22:08:16 +0200

    add Ball add Color add Lottery

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+
+    public Ball getRandomBall() {
+        Random random = new Random();
+        int valueBoll = random.nextInt(101);
+
+        ColorSupplier colorRandomBall = new ColorSupplier();
+        Color color = colorRandomBall.getRandomColor();
+
+        Ball ball = new Ball();
+        ball.setColor(color);
+        ball.setNumber(valueBoll);
+
+
+
+        return ball;
+    }
+}
